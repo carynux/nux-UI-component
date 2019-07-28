9f09e28cea91d132f732cba9e0c814fb4da3deba
cary <carynux@gmail.com>
2019-07-28 13:20:39 +0800

    加入icon和iconPosition

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,18 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button"  :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon" aria-hidden="true">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
-    export default { }
+    export default {
+        props:['icon','iconPosition']
+    }
 </script>
 
 <style lang="scss">
@@ -14,16 +23,40 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
         &:hover {
             border-color: var(--border-color-hover);
         }
+
         &:active {
             background-color: var(--button-active-bg);
         }
+
         &:focus {
             outline: none;
         }
+        > .content{
+            order:2
+        }
+        > .icon{
+            order:1;
+            margin-right: .3em;
+        }
+        &.icon-right{
+            > .content{
+                order:1
+            }
+            > .icon{
+                order:2;
+                margin-left: .3em;
+                margin-right: 0;
+            }
+        }
         /*&表示当前的选择器*/
+        /*>表示选择子代，孙代不选择*/
     }
 
 </style>
